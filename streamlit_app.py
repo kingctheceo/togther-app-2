7c7ba9ca79142e8a63f6a01f0fbeab1c8282ba54
kingctheceo <garychristopher97@gmail.com>
2025-06-04 23:59:26 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1057,7 +1057,7 @@ if not check_login():
                 st.warning("Enter your username to continue.")
 
 # Main app container
-st.markdown('<div class="app-container">', unsafe_allow_html=True)
+    st.markdown('<div class="app-container">', unsafe_allow_html=True)
 
 # Header
 user = st.session_state.get("current_user")
