8db7105999df90dc42aa39319b98a35947bf1dfe
kingctheceo <garychristopher97@gmail.com>
2025-06-04 23:04:29 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1695,7 +1695,6 @@ st.markdown('</div>', unsafe_allow_html=True)  # Close app-container
 # Add bottom spacing for navigation
 st.markdown("<div style='height: 100px;'></div>", unsafe_allow_html=True)
 # Sign In Flow
-with tab3:
     st.markdown("### Welcome Back")
     st.markdown("""
     <div class="auth-container">
